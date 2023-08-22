243cfba577eaa7f8a8f11e34681ad51579c19631
Kvcss <kaiovsilva60@gmail.com>
2023-08-22 11:47:04 -0300

    correção da api

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 
 // models
